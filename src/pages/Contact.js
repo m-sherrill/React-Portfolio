47f95e6167adf0937ca0389c05edf43fe88aee13
Morgan Sherrill <mo.sherrill@outlook.com>
2022-02-16 15:02:38 -0800

    working on getting my tooltip working on the contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,34 +1,40 @@
 import React from 'react'
 import '../App.css';
 import { Link } from "react-router-dom"
+import ReactTooltip from "react-tooltip"
 import { Container } from 'react-materialize'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
 const Contact = () => {
+
+
+
   return (
-  
-      <>
+
+    <>
       <Header />
       <Container>
-      <div id="contactMeDesc">
-      Connect with me!
+        <div id="contactMeDesc">
+          Connect with me!
         </div>
-      <div id="contactMeContent">
-        <a href="mailto:mo.sherrill@outlook.com" target="_blank" rel="noreferrer" ><img src="assets/images/emailicon.png"
-          className="tooltipped" data-position="top" data-tooltip="Email" alt="email icon" /></a>
-        <a href="http://github.com/m-sherrill" target="_blank" rel="noreferrer" ><img src="assets/images/githubicon.png"
-          className="tooltipped" data-position="top" data-tooltip="GitHub" alt="github icon" /></a>
-        <a href="https://www.linkedin.com/in/morgan-sherrill-9a838b28/" target="_blank" rel="noreferrer" ><img
-          src="assets/images/linkedinicon.png" className="tooltipped" data-position="top" data-tooltip="LinkedIn" alt="linkedin icon" /></a>
-        <a href="msherrillresume.pdf" download rel="noreferrer" ><img src="assets/images/resume.png"
-          className="tooltipped" data-position="top" data-tooltip="Resume" alt="resume icon" /></a><br /><br />
-        <Link className="btn-floating btn-large introLink" to="/"><i className="material-icons" alt="home button">home</i></Link>
-      </div>
-    </Container>
-    <Footer />
+
+        <div id="contactMeContent">
+          <ReactTooltip place="top" type="success" effect="solid" />
+          <a href="mailto:mo.sherrill@outlook.com" target="_blank" rel="noreferrer" ><img src="assets/images/emailicon.png" alt="email icon" data-tip="Email" data-background-color="darkGreen" /></a>
+
+          <a href="http://github.com/m-sherrill" target="_blank" rel="noreferrer" ><img src="assets/images/githubicon.png"
+            data-tip="GitHub" alt="github icon" /></a>
+          <a href="https://www.linkedin.com/in/morgan-sherrill-9a838b28/" target="_blank" rel="noreferrer" ><img
+            src="assets/images/linkedinicon.png" data-tip="LinkedIn" alt="linkedin icon" /></a>
+          <a href="msherrillresume.pdf" download rel="noreferrer" ><img src="assets/images/resume.png"
+            data-tip="Resume" alt="resume icon" /></a><br /><br />
+          <Link className="btn-floating btn-large introLink" to="/"><i className="material-icons" alt="home button">home</i></Link>
+        </div>
+      </Container>
+      <Footer />
     </>
-  
+
   )
 }
 
