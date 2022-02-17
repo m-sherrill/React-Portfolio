847b0137eaead74da72013dc3fa9e9e8b94a3873
Morgan Sherrill <mo.sherrill@outlook.com>
2022-02-16 23:05:55 -0800

    started the layout on tunespace, weatherapp, and scheduler project report

--- src/pages/subpages/Tunespace.js
+++ src/pages/subpages/Tunespace.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import '../../ProjectStyles.css';
 import { useNavigate, Link } from "react-router-dom"
 import { Container, Button, Row, Col } from 'react-materialize'
+import ProjectsHeader from '../../components/ProjectsHeader'
+import ProjectsFooter from '../../components/ProjectsFooter'
 
 const Tunespace = () => {
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
+
   return (
+    <>
+    <ProjectsHeader />
     <div>Tunespace</div>
+    <ProjectsFooter />
+    </>
   )
 }
 
